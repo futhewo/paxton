1bf1d6ea53f7975ad2a15e7ea34c5d0ae7b6764f
fuzzTheWorld <fuzztheworld@gmail.com>
2017-10-31 16:12:44 +0100

    Add a setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='paxton',
+      version='0.01',
+      description='A tool that aim to produce outputs similar to a given input.',
+      url='http://github.com/futhewo/paxton',
+      author='FuzzTheWorld',
+      author_email='fuzztheworld@gmail.com',
+      license='Apache2',
+      packages=['paxton'],
+      zip_safe=False)
