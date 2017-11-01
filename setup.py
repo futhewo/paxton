02bf34587985c5aaf8ba06bd567a6c73346b00e5
fuzzTheWorld <fuzztheworld@gmail.com>
2017-11-01 11:00:48 +0100

    Put the tool in version 0.2.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='paxton',
-      version='0.1',
-      description='A tool that aim to produce outputs similar to a given input.',
+      version='0.2',
+      description='A basic data structure learner and generator.',
       url='http://github.com/futhewo/paxton',
       author='FuzzTheWorld',
       author_email='fuzztheworld@gmail.com',
